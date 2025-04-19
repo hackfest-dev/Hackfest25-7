52e17e6eaba189493e4b8d91075f6f3cf42ae978
Ananya-yes <159869549+Ananya-yes@users.noreply.github.com>
2025-04-20 04:53:25 +0530

    Delete src/lib directory

--- src/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = 'https://gsyqmgymkajiccoybdoe.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzeXFtZ3lta2FqaWNjb3liZG9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExMDcyMzcsImV4cCI6MjA1NjY4MzIzN30.s_qZYPayLf5lwcYZeaQnhgyyUcOgCrcHMd2lst-OJuQ';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
